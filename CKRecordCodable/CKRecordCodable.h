5576f0764fe7b58f0d39e9c64b29c76f403c9688
Paul Cadman <git@paulcadman.io>
2017-09-30 22:01:01 +0100

    Initial Commit

---
+++ CKRecordCodable/CKRecordCodable.h
@@ -0,0 +1,19 @@
+//
+//  CKRecordCodable.h
+//  CKRecordCodable
+//
+//  Created by Paul Cadman on 30/09/2017.
+//  Copyright © 2017 Paul Cadman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CKRecordCodable.
+FOUNDATION_EXPORT double CKRecordCodableVersionNumber;
+
+//! Project version string for CKRecordCodable.
+FOUNDATION_EXPORT const unsigned char CKRecordCodableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CKRecordCodable/PublicHeader.h>
+
+
---
+++ CKRecordCodable/CKRecordDecoder.swift
@@ -0,0 +1,9 @@
+//
+//  CKRecordDecoder.swift
+//  CKRecordCodable
+//
+//  Created by Paul Cadman on 30/09/2017.
+//  Copyright © 2017 Paul Cadman. All rights reserved.
+//
+
+import Foundation
